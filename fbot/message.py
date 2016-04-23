e475f2fccd75ef5c14906467de19425334d5aa3b
KOnstantin Shamko <konstantin.shamko@gmail.com4>
2016-04-23 12:11:00 +0300

    teaching bot to talk

--- fbot/message.py
+++ fbot/message.py
@@ -6,16 +6,16 @@ class Message:
     fb_token = 'EAAX6dRFN4A4BAN4BUe3PWoiSwajNVkDkADu9A2N23yrf8eAAis6djDVFnU6buxLgyw6cPIYB0M2jSPPuYCv3GyuZBZCiRqFt14lSZCmZBllWEzkI2MSs1MX7Mm9KipsI0VfVnOdxyxlAMnSQPWBUOCet8CSByZBXlkB9Q9VGPRQZDZD'
     fb_reply_url = 'https://graph.facebook.com/v2.6/me/messages'
     bot_message = ''
-    message = {}
+    message = {"text": ""}
     sender = 0
     
     
     def __init__(self, message_json):
         self.bot_message = json.loads(message_json)['entry'][0]
-        self.message = self.bot_message['messaging'][0]#['message']
         
-        print self.message
-        
+        if "message" in self.bot_message['messaging'][0]:
+            self.message = self.bot_message['messaging'][0]['message']
+       
         self.sender = self.bot_message['messaging'][0]['sender']['id']     
         #self.message = self.bot_message.message
     
