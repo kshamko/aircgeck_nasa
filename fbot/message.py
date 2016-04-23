88d9d198210330cc6c2c4e3fd9d5b177caa6e4d5
KOnstantin Shamko <konstantin.shamko@gmail.com4>
2016-04-23 17:55:51 +0300

    save user

--- fbot/message.py
+++ fbot/message.py
@@ -7,7 +7,7 @@ class Message:
     
     fb_token = 'EAAX6dRFN4A4BAN4BUe3PWoiSwajNVkDkADu9A2N23yrf8eAAis6djDVFnU6buxLgyw6cPIYB0M2jSPPuYCv3GyuZBZCiRqFt14lSZCmZBllWEzkI2MSs1MX7Mm9KipsI0VfVnOdxyxlAMnSQPWBUOCet8CSByZBXlkB9Q9VGPRQZDZD'
     fb_reply_url = 'https://graph.facebook.com/v2.6/me/messages' 
-    fb_user_url = 'https://graph.facebook.com/v2.6/%s?fields=first_name,last_name,profile_pic,current_location&access_token=%s'
+    fb_user_url = 'https://graph.facebook.com/v2.6/%s?fields=first_name,last_name,profile_pic&access_token=%s'
     bot_message = ''
     message = {"text": None}
     sender = 0
