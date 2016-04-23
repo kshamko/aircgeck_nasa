18d0531bb3b58a4978fc6d13e85cad5732a65b88
KOnstantin Shamko <konstantin.shamko@gmail.com4>
2016-04-23 12:44:31 +0300

    teaching bot to talk

--- fbot/message.py
+++ fbot/message.py
@@ -6,12 +6,14 @@ class Message:
     fb_token = 'EAAX6dRFN4A4BAN4BUe3PWoiSwajNVkDkADu9A2N23yrf8eAAis6djDVFnU6buxLgyw6cPIYB0M2jSPPuYCv3GyuZBZCiRqFt14lSZCmZBllWEzkI2MSs1MX7Mm9KipsI0VfVnOdxyxlAMnSQPWBUOCet8CSByZBXlkB9Q9VGPRQZDZD'
     fb_reply_url = 'https://graph.facebook.com/v2.6/me/messages'
     bot_message = ''
-    message = {"text": ""}
+    message = None
     sender = 0
     
     
     def __init__(self, message_json):
         self.bot_message = json.loads(message_json)['entry'][0]
+        
+        print message_json
         
         if 'message' in self.bot_message['messaging'][0]:
             self.message = self.bot_message['messaging'][0]['message']
@@ -22,32 +24,30 @@ class Message:
     def reply(self):
         reply = self.process_input()
         
-        data = {
-            "recipient": {"id": self.sender},
-            "message": {"text": reply},
-        }
-        #print json.dumps(data)
-        #json_data = json.dump(data)
-        
-        fb_msg_url = self.fb_reply_url + '/?access_token=' + self.fb_token
-        #urllib2.urlopen(fb_msg_url, json.dumps(data))
-         
-        request = urllib2.Request(fb_msg_url, json.dumps(data), {'Content-Type': 'application/json'})
-        #response = urllib2.urlopen(request)
-        
-        try: 
-           response = urllib2.urlopen(request)
-        except Exception:
-           print 'answer error'
-                   
+        if reply is not None:
+            data = {
+                "recipient": {"id": self.sender},
+                "message": {"text": reply},
+            }
+            
+            fb_msg_url = self.fb_reply_url + '/?access_token=' + self.fb_token
+            request = urllib2.Request(fb_msg_url, json.dumps(data), {'Content-Type': 'application/json'})
+            try: 
+               urllib2.urlopen(request)
+            except Exception:
+               print 'answer error'
+        else:
+            reply = ''           
          
         return reply;
     
     def process_input(self):
-        message = self.message['text'].lower()
-        reply = 'Hmmm...'
-        if message == 'hello' or message == 'hey':
-            reply = 'Hello! How do you feel today?'
+        reply = None
+        if self.message is not None:
+            message = self.message['text'].lower()
+            reply = 'Hmmm...'
+            if message == 'hello' or message == 'hey':
+                reply = 'Hello! How do you feel today?'
             
             
         return reply    
