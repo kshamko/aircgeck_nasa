f7fe2607de63d7c8d6f41aeb1e5c7ad882a6de1c
KOnstantin Shamko <konstantin.shamko@gmail.com4>
2016-04-23 13:32:16 +0300

    teaching bot to talk

--- fbot/message.py
+++ fbot/message.py
@@ -4,7 +4,7 @@ import urllib2
 class Message:
     
     fb_token = 'EAAX6dRFN4A4BAN4BUe3PWoiSwajNVkDkADu9A2N23yrf8eAAis6djDVFnU6buxLgyw6cPIYB0M2jSPPuYCv3GyuZBZCiRqFt14lSZCmZBllWEzkI2MSs1MX7Mm9KipsI0VfVnOdxyxlAMnSQPWBUOCet8CSByZBXlkB9Q9VGPRQZDZD'
-    fb_reply_url = 'https://graph.facebook.com/v2.6/me/messages'
+    fb_reply_url = 'https://graph.facebook.com/v2.6/me/messages' 
     bot_message = ''
     message = None
     sender = 0
@@ -21,25 +21,19 @@ class Message:
         self.sender = self.bot_message['messaging'][0]['sender']['id']     
         #self.message = self.bot_message.message
     
+    
+    
     def reply(self):
         reply = self.process_input()
         
         if reply is not None:
-            data = {
-                "recipient": {"id": self.sender},
-                "message": {"text": reply},
-            }
-            
-            fb_msg_url = self.fb_reply_url + '/?access_token=' + self.fb_token
-            request = urllib2.Request(fb_msg_url, json.dumps(data), {'Content-Type': 'application/json'})
-            try: 
-               urllib2.urlopen(request)
-            except Exception:
-               print 'answer error'
+            self._send_template_reply('xx')
         else:
             reply = ''           
          
         return '';
+    
+    
     
     def process_input(self):
         reply = None
@@ -50,4 +44,70 @@ class Message:
                 reply = 'Hello! How do you feel today?'
             
             
-        return reply    
+        return reply
+    
+    def _send_template_reply(self, text):
+        data = {
+             "message":{
+                "attachment":{
+                   "type":"template",
+                   "payload":{
+                        "template_type":"button",
+                        "text":"What do you want to do next?",
+                        "buttons":[
+                            {
+                            "type":"web_url",
+                            "url":"https://petersapparel.parseapp.com",
+                            "title":"Show Website"
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+
+        print 'send quiz'
+        status = self._send_reply_api(data)
+        
+        print status
+        return status
+    
+    
+    def _send_image_reply(self, text, img):
+        data = {
+            "message": {"text": text},
+        }     
+        
+        return self._send_reply_api(data)
+    
+    
+    def _send_text_reply(self, text):
+        data = {
+            "message": {"text": text},
+        }     
+        
+        return self._send_reply_api(data)
+    
+    
+    def _send_reply_api(self, data):
+        
+        data['recipient'] = {"id": self.sender}
+        status = False
+        fb_msg_url = self.fb_reply_url + '/?access_token=' + self.fb_token
+        
+        print json.dumps(data)
+        
+        request = urllib2.Request(fb_msg_url, json.dumps(data), {'Content-Type': 'application/json'})
+        #urllib2.urlopen(request)
+        
+        try: 
+            urllib2.urlopen(request)
+            status = True
+        except Exception:
+            status = False
+           
+        return status   
+           
+    
+    
+        
