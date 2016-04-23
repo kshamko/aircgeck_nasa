b6875ecf4819ad76bf5ceb39216d6bc9c0b03758
KOnstantin Shamko <konstantin.shamko@gmail.com4>
2016-04-23 11:16:48 +0300

    teaching bot to talk

--- fbot/message.py
+++ fbot/message.py
@@ -9,7 +9,7 @@ class Message:
     
     def __init__(self, message_json):
         self.bot_message = json.loads(message_json)['entry']
-        self.message = self.bot_message['messaging'][0]['message']
+        self.message = self.bot_message['messaging'][0]['message'][0]
                 
         #self.message = self.bot_message.message
     
